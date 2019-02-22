5565627b9147409fa6e78eba8d17a0c30e8cb336
ztlsir <584821699@qq.com>
2019-02-23 00:08:22 +0800

    init project

---
+++ SampleEvent/SampleEvent/IEventHandleProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleEvent
+{
+    public interface IEventHandleProvider
+    {
+        void RegisterEvents();
+        IEnumerable<IEventHandler<TSampleEvent>> GetEventHandlers<TSampleEvent>() where TSampleEvent : ISampleEvent;
+        IEnumerable<Action<TSampleEvent>> GetHandles<TSampleEvent>() where TSampleEvent : ISampleEvent;
+    }
+}
---
+++ SampleEvent/SampleEvent/IEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleEvent
+{
+    public interface IEventHandler<TSampleEvent> where TSampleEvent : ISampleEvent
+    {
+        void Handle(TSampleEvent sampleEvent);
+    }
+}
